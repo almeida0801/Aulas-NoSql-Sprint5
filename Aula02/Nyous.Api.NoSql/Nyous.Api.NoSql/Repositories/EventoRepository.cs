d1243964183f95cf73514d024105beb046f545c3
FIC <FIC@info.local>
2020-12-09 17:58:04 -0200

    Aula02 em andamento

---
+++ Aula02/Nyous.Api.NoSql/Nyous.Api.NoSql/Repositories/EventoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nyous.Api.NoSql.Repositories
+{
+    public class EventoRepository
+    {
+    }
+}
